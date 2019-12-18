91a6f978a69daeef9e5ecb9e649f8a1d227b8887
Fiaccadori Nicol <fiaccadorin@imac-r06.pascal.local>
2019-12-18 09:29:51 +0100

    cosa scrivo

---
+++ sketch2.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+
+  let sketch = function(p) {
+    p.setup = function(){
+      p.createCanvas(100, 100);
+      p.background(0);
+    }
+  };
+  new p5(sketch, window.document.getElementById('container'));
+ 
